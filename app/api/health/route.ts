de34f0632f11da67c64f489c0c229c87f207cd95
Dinesh <contactdineshmoorthy@gmail.com>
2025-04-18 20:29:21 +0100

    Update route.ts
    deployment fix

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -1,6 +1,9 @@
 
 import { NextResponse } from 'next/server'
 
+export const dynamic = 'force-static'; // Forces static generation
+export const revalidate = false; // Optional: Ensures no revalidation
+
 export async function GET() {
   return NextResponse.json({ status: 'ok' })
 }
